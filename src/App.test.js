1c57325dc944b1885ffdefc349b0ae6ed50a1907
Felipe Escorel <felipe.escorel@gmail.com>
2022-08-03 12:57:00 -0300

    adds test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Footer, Header, MainInformation, PeriodForecast } from './components/CityWeather/Forecast';
+import TownSelect from './components/TownSelect/TownSelect';
+import {api} from './services/weatherapi/api'
+import {transformData} from './services/weatherapi/getForecast'
+import {useEffect} from 'react'
+import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let city = "Recife"
+const fetchData = () => {
+ return api.get('', {
+    params: {
+      q: city
+    }
+  }).then(({data}) => transformData(data))
+}
+
+test('renders town select', () => {
+  render(<TownSelect />);
+  const element = screen.getByText(/Recife/i);
+  expect(element).toBeInTheDocument();
+});
+
+test('renders city weather main information component', async () => {
+  let data = await fetchData()
+  render(<MainInformation data={data}/>)
+  const element = screen.getByAltText(/weather-icon/i)
+  expect(element).toBeInTheDocument()
+
+});
+
+test('renders city weather header component', async () => {
+  let data = await fetchData()
+  render(<Header data={data}/>)
+  const cityName = screen.getByText(/Recife/i)
+  const cityWeather = screen.getByText(data.condition.text.toLowerCase())
+  expect(cityName).toBeInTheDocument();
+  expect(cityWeather).toBeInTheDocument();
+});
+
+test('renders city weather footer component', async () => {
+  let data = await fetchData()
+  render(<Footer data={data}/>)
+  const element = screen.getByText(/sunrise/i)
+  expect(element).toBeInTheDocument();
+});
+
+test('renders city weather period forecast component', async () => {
+  let data = await fetchData()
+  render(<PeriodForecast data={data}/>)
+  const elements = screen.getAllByAltText(/weather-icon/i).length
+  expect(elements).toEqual(4)
 });
